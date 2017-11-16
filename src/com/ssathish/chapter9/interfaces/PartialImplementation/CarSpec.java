5f40febad58d395d7d105177fdf87f50bebdb9c3
Sathishkumar Subramanian <ssathishkumar@athenahealth.com>
2017-11-16 22:20:14 +0530

    Carspec partial implementation of the interfaces

---
+++ src/com/ssathish/chapter9/interfaces/PartialImplementation/CarSpec.java
@@ -0,0 +1,7 @@
+package com.ssathish.chapter9.interfaces.PartialImplementation;
+
+public interface CarSpec {
+    int height(String brand);
+    int width(String brand);
+    void engine(String brand);
+}
