656d5e081b3ba9727bdc50cb1887c2361ded9078
Sathishkumar Subramanian <ssathishkumar@athenahealth.com>
2017-12-04 12:49:09 +0530

    Recent changes to the variables in interfaces

---
+++ src/com/ssathish/chapter9/interfaces/VariablesInInterfaces/AskMe.java
@@ -0,0 +1,23 @@
+package src.com.ssathish.chapter9.interfaces.VariablesInInterfaces;
+
+public class AskMe implements SharedConstants {
+    static void answer(int result) {
+        switch (result) {
+            case NO:
+                System.out.println("Result is: 'NO'");
+                break;
+            case YES:
+                System.out.println("Result is: 'YES'");
+                break;
+            case MAYBE:
+                System.out.println("Result is: 'MAYBE'");
+        }
+    }
+
+    public static void main(String[] args) {
+        Question q = new Question();
+
+        AskMe.answer(q.ask());
+        AskMe.answer(q.ask());
+    }
+}
---
+++ src/com/ssathish/chapter9/interfaces/VariablesInInterfaces/Question.java
@@ -0,0 +1,21 @@
+package src.com.ssathish.chapter9.interfaces.VariablesInInterfaces;
+
+import java.util.Random;
+
+public class Question implements SharedConstants {
+    Random rand = new Random();
+
+    int ask() {
+        int prob = (int) (100 * rand.nextDouble());
+        if (prob < 30) {
+            return NO;
+        }
+        else if ((prob > 30) && (prob < 50)) {
+            return YES;
+        }
+        else if ((prob > 60 && (prob < 80))) {
+            return MAYBE;
+        }
+        return NEVER;
+    }
+}
