737ab94971551aa541048170de7d92b539fe9275
Sathish Kumar <neosathishgct@gmail.com>
2017-12-11 02:15:35 +0530

    Creating thread from Runnable interface

---
+++ src/com/ssathish/chapter11/CreateRunnableThread.java
@@ -0,0 +1,42 @@
+package com.ssathish.chapter11;
+
+class CreateThreadFromRunnable implements Runnable {
+
+    Thread t;
+
+    public CreateThreadFromRunnable() {
+        t = new Thread(this, "Demo Thread");
+        System.out.println("Child thread" + t);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("child thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println("Child Thread interrupted!!");
+        }
+
+        System.out.println("Exiting child thread");
+    }
+}
+
+public class CreateRunnableThread {
+    public static void main(String[] args) {
+        new CreateThreadFromRunnable();
+
+        try {
+            for(int i = 5; i > 0; i--) {
+                System.out.println("Main Thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println("Main Thread interrupted");
+        }
+        System.out.println("Main thread exiting");
+    }
+}
